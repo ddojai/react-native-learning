860f77e2bfbc29f516a8f64ef87ee56fddf74da0
ddojai <heungyeol87@gmail.com>
2022-11-24 19:44:43 +0900

    2.4 JSX 문법

--- LearnReactNative/App.js
+++ LearnReactNative/App.js
@@ -3,9 +3,13 @@ import {SafeAreaView} from 'react-native';
 import Greeting from './components/Greeting';
 
 const App = () => {
+  const name = 'JSX';
   return (
     <SafeAreaView>
-      <Greeting />
+      {/* 주석을 작성해봅시다. */}
+      <Greeting
+        name={name} // 이름을 설정하기
+      />
     </SafeAreaView>
   );
 };
