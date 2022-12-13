e1bb4ae4d05868a5ce94fd647e25f7531b871267
ddojai <heungyeol87@gmail.com>
2022-12-13 23:06:22 +0900

    3.2.3

--- TodoApp/components/AddTodo.js
+++ TodoApp/components/AddTodo.js
@@ -1,5 +1,13 @@
 import React, {useState} from 'react';
-import {View, StyleSheet, TextInput} from 'react-native';
+import {
+  View,
+  StyleSheet,
+  TextInput,
+  Image,
+  TouchableOpacity,
+  Platform,
+  TouchableNativeFeedback,
+} from 'react-native';
 
 function AddTodo() {
   const [text, setText] = useState('');
@@ -12,22 +20,56 @@ function AddTodo() {
         value={text}
         onChangeText={setText}
       />
+      {Platform.OS === 'ios' ? (
+        <TouchableOpacity activeOpacity={0.5}>
+          <View style={styles.buttonStyle}>
+            <Image
+              source={require('../assets/icons/add_white/add_white.png')}
+            />
+          </View>
+        </TouchableOpacity>
+      ) : (
+        <View style={styles.circleWrapper}>
+          <TouchableNativeFeedback>
+            <View style={styles.buttonStyle}>
+              <Image
+                source={require('../assets/icons/add_white/add_white.png')}
+              />
+            </View>
+          </TouchableNativeFeedback>
+        </View>
+      )}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   block: {
+    backgroundColor: 'white',
     height: 64,
     paddingHorizontal: 16,
     borderColor: '#bdbdbd',
     borderTopWidth: 1,
     borderBottomWidth: 1,
-    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
   },
   input: {
+    flex: 1,
     fontSize: 16,
     paddingVertical: 8,
+  },
+  buttonStyle: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 48,
+    height: 48,
+    backgroundColor: '#26a69a',
+    borderRadius: 24,
+  },
+  circleWrapper: {
+    overflow: 'hidden',
+    borderRadius: 24,
   },
 });
 
