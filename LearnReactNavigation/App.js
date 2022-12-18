4b75f794bcbcbdb1d2f91260c754df5f10ed66f7
ddojai <heungyeol87@gmail.com>
2022-12-18 15:48:51 +0900

    5.2.5.2  헤더 스타일 변경

--- LearnReactNavigation/App.js
+++ LearnReactNavigation/App.js
@@ -10,8 +10,31 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Detail" component={DetailScreen} />
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            title: '홈',
+            // Header 블록에 대한 스타일
+            headerStyle: {
+              backgroundColor: '#29b6f6',
+            },
+            // Header의 텍스트, 버튼들 색상
+            headerTintColor: '#ffffff',
+            // 타이틀 텍스트의 스타일
+            headerTitleStyle: {
+              fontWeight: 'bold',
+              fontSize: 20,
+            },
+          }}
+        />
+        <Stack.Screen
+          name="Detail"
+          component={DetailScreen}
+          options={({route}) => ({
+            title: `상세 정보 - ${route.params.id}`,
+          })}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
