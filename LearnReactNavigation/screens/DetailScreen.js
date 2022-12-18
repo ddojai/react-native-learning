dd9c7c83638a4c5b0d2eca79664d69d82a4fa43b
ddojai <heungyeol87@gmail.com>
2022-12-18 15:00:14 +0900

    5.2.3 라우트 파라미터

--- LearnReactNavigation/screens/DetailScreen.js
+++ LearnReactNavigation/screens/DetailScreen.js
@@ -1,12 +1,27 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet, Button} from 'react-native';
 
-function DetailScreen() {
+function DetailScreen({route, navigation}) {
   return (
-    <View>
-      <Text>Detail</Text>
+    <View style={styles.block}>
+      <Text style={styles.text}>id: {route.params.id}</Text>
+      <Button
+        title="다음"
+        onPress={() => navigation.push('Detail', {id: route.params.id + 1})}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  block: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 48,
+  },
+});
 
 export default DetailScreen;
