1db830f68a07e04e283ba09d8c05f06be92b5675
ddojai <heungyeol87@gmail.com>
2022-12-18 15:23:40 +0900

    5.2.5.1  타이틀 텍스트 변경하기

--- LearnReactNavigation/screens/HomeScreen.js
+++ LearnReactNavigation/screens/HomeScreen.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {View, Button} from 'react-native';
 
 function HomeScreen({navigation}) {
+  useEffect(() => {
+    navigation.setOptions({title: '홈'});
+  }, [navigation]);
+
   return (
     <View>
       <Button
