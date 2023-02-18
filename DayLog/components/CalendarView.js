aa9a04163b1dcaba73e1cc9f7f69e98503c3f660
ddojai <heungyeol87@gmail.com>
2023-02-18 16:00:27 +0900

    7.5.1 달력에 표시하기

--- DayLog/components/CalendarView.js
+++ DayLog/components/CalendarView.js
@@ -3,7 +3,31 @@ import {Calendar} from 'react-native-calendars';
 import {StyleSheet} from 'react-native';
 
 function CalendarView() {
-  return <Calendar style={styles.calendar} />;
+  // 현재 연/월 사용하기
+  const markedDates = {
+    '2023-02-16': {
+      selected: true,
+    },
+    '2023-02-17': {
+      marked: true,
+    },
+    '2023-02-18': {
+      marked: true,
+    },
+  };
+
+  return (
+    <Calendar
+      style={styles.calendar}
+      markedDates={markedDates}
+      theme={{
+        selectedDayBackgroundColor: '#009688',
+        arrowColor: '#009688',
+        dotColor: '#009688',
+        todayTextColor: '#009688',
+      }}
+    />
+  );
 }
 
 const styles = StyleSheet.create({
