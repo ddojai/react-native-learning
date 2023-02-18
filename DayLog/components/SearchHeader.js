01fa8ae069a5fff005686455ecb8d0ecfac9257c
ddojai <heungyeol87@gmail.com>
2023-02-18 14:23:52 +0900

    7.4.3 검색 컴포넌트  UI 구성

--- DayLog/components/SearchHeader.js
+++ DayLog/components/SearchHeader.js
@@ -1,15 +1,40 @@
 import React from 'react';
-import {StyleSheet, useWindowDimensions, View} from 'react-native';
+import {
+  Pressabe,
+  StyleSheet,
+  TextInput,
+  useWindowDimensions,
+  View,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 function SearchHeader() {
   const {width} = useWindowDimensions();
-
-  return <View style={[styles.block, {width: width - 32, height: 24}]} />;
+  return (
+    <View style={[styles.block, {width: width - 32}]}>
+      <TextInput
+        style={styles.input}
+        placeholder="검색어를 입력하세요"
+        autoFocus
+      />
+      <Pressabe
+        style={({pressed}) => [styles.button, pressed && {opacity: 0.5}]}>
+        <Icon name="cancel" size={20} color="#9e9e9e" />
+      </Pressabe>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   block: {
-    backgroundColor: 'blue',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  input: {
+    flex: 1,
+  },
+  button: {
+    marginLeft: 8,
   },
 });
 
