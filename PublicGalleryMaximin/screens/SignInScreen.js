1809cb99bd5b09ee0616d5c0d3ae2d64d10f9e56
ddojai <heungyeol87@gmail.com>
2023-02-20 00:21:28 +0900

    8.2.2 비밀번호 마스킹, 이메일 입력 키보드 적용

--- PublicGalleryMaximin/screens/SignInScreen.js
+++ PublicGalleryMaximin/screens/SignInScreen.js
@@ -38,18 +38,24 @@ function SignInScreen({navigation, route}) {
             placeholder="이메일"
             value={form.email}
             onChangeText={createChangeTextHandler('email')}
+            autoCapitalize="none"
+            autoCorrect={false}
+            autoCompleteType="email"
+            keyboardType="email-address"
           />
           <BorderedInput
             placeholder="비밀번호"
             hasMarginBottom={isSignUp}
             value={form.password}
             onChangeText={createChangeTextHandler('password')}
+            secureTextEntry
           />
           {isSignUp && (
             <BorderedInput
               placeholder="비밀번호 확인"
               value={form.confirmPassword}
               onChangeText={createChangeTextHandler('confirmPassword')}
+              secureTextEntry
             />
           )}
           <View style={styles.buttons}>
