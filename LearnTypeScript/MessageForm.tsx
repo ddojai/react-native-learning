b9a0fa92bcdfefcb0420b7da103a5a1b94934408
ddojai <heungyeol87@gmail.com>
2023-05-05 15:39:34 +0900

    12.3.2 useState 사용하기

---
+++ LearnTypeScript/MessageForm.tsx
@@ -0,0 +1,35 @@
+import React, {useState} from 'react';
+import {Button, Text, TextInput, View} from 'react-native';
+
+function MessageForm() {
+  const [text, setText] = useState('');
+  const [lastMessage, setLastMessage] = useState<{
+    message: string;
+    date: Date;
+  } | null>(null);
+
+  const onPress = () => {
+    setLastMessage({
+      message: text,
+      date: new Date(),
+    });
+    setText('');
+  };
+
+  return (
+    <View>
+      <TextInput value={text} onChangeText={setText} />
+      <Button title="PRESS ME" onPress={onPress} />
+      {lastMessage && (
+        <View>
+          <Text>
+            마지막 메시지: {lastMessage.message} (
+            {lastMessage.date.toLocaleString()})
+          </Text>
+        </View>
+      )}
+    </View>
+  );
+}
+
+export default MessageForm;
