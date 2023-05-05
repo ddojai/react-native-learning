aebdccbc808fefe6248529c6580f3b787dd03b71
ddojai <heungyeol87@gmail.com>
2023-05-05 15:52:29 +0900

    12.3.3 useRef 사용하기

--- LearnTypeScript/MessageForm.tsx
+++ LearnTypeScript/MessageForm.tsx
@@ -1,24 +1,36 @@
-import React, {useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {Button, Text, TextInput, View} from 'react-native';
 
 function MessageForm() {
   const [text, setText] = useState('');
   const [lastMessage, setLastMessage] = useState<{
+    id: number;
     message: string;
     date: Date;
   } | null>(null);
+  const nextId = useRef<number>(1);
+  const inputRef = useRef<TextInput | null>(null);
 
   const onPress = () => {
     setLastMessage({
       message: text,
       date: new Date(),
+      id: nextId.current,
     });
     setText('');
+    nextId.current += 1;
   };
+
+  useEffect(() => {
+    if (!inputRef.current) {
+      return;
+    }
+    inputRef.current.focus();
+  }, []);
 
   return (
     <View>
-      <TextInput value={text} onChangeText={setText} />
+      <TextInput value={text} onChangeText={setText} ref={inputRef} />
       <Button title="PRESS ME" onPress={onPress} />
       {lastMessage && (
         <View>
