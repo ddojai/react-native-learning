c96b4c5236a863d85d7ad70f8589f3f9a06bd310
ddojai <heungyeol87@gmail.com>
2023-05-05 15:09:57 +0900

    12.2.6.3 클래스에서 Generic 사용하기

--- LearnTypeScript/learn.ts
+++ LearnTypeScript/learn.ts
@@ -178,3 +178,25 @@ const placeItem: Item<Place> = {
     location: 'Korea',
   },
 };
+
+class Queue<T> {
+  list: T[] = [];
+
+  get length() {
+    return this.list.length;
+  }
+
+  enqueue(item: T) {
+    this.list.push(item);
+  }
+
+  dequeue() {
+    return this.list.shift();
+  }
+}
+
+const queue = new Queue<number>();
+queue.enqueue(0);
+queue.enqueue(1);
+const first = queue.dequeue();
+const second = queue.dequeue();
