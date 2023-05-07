a5b652563ce395ea13aa13264ceda7cbf6eb0f7d
ddojai <heungyeol87@gmail.com>
2023-05-07 15:42:58 +0900

    13.4.2 TodoApp 틀 잡아주기

--- StateManagement/App.tsx
+++ StateManagement/App.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import {Provider} from 'react-redux';
 import {legacy_createStore as createStore} from 'redux';
 import rootReducer from './slices';
-import AuthApp from './components/AuthApp';
+import TodoApp from './components/TodoApp';
 
 const store = createStore(rootReducer);
 
 function App() {
   return (
     <Provider store={store}>
-      <AuthApp />
+      <TodoApp />
     </Provider>
   );
 }
