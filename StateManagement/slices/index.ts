786cbf8cbc382e39b146d2fe6d0259d5226a4524
ddojai <heungyeol87@gmail.com>
2023-05-07 14:10:11 +0900

    13.3.6 useSelector의 DefaultRootState 지정하기

--- StateManagement/slices/index.ts
+++ StateManagement/slices/index.ts
@@ -6,7 +6,10 @@ const rootReducer = combineReducers({
 });
 
 // rootReducer 함수의 반환값 타입을 RootState type alias로 지정
-
 export type RootState = ReturnType<typeof rootReducer>;
+
+declare module 'react-redux' {
+  interface DefaultRootState extends RootState {}
+}
 
 export default rootReducer;
