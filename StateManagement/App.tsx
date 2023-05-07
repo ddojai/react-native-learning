8c36fb621277a67cd02a3645f799a0ed32d885a9
ddojai <heungyeol87@gmail.com>
2023-05-07 16:56:23 +0900

    13.5.4 PostsApp 틀 잡아주기

--- StateManagement/App.tsx
+++ StateManagement/App.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import {Provider} from 'react-redux';
 import rootReducer from './slices';
-import TodoApp from './components/TodoApp';
 import {configureStore} from '@reduxjs/toolkit';
+import PostsApp from './components/PostsApp';
 
 const store = configureStore({reducer: rootReducer});
 
 function App() {
   return (
     <Provider store={store}>
-      <TodoApp />
+      <PostsApp />
     </Provider>
   );
 }
