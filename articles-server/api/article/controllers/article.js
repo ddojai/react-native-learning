ea82fa70d5b13adf3b3500bd3a64eb724de9326e
ddojai <heungyeol87@gmail.com>
2023-05-10 23:43:18 +0900

    14.5.3 게시글 삭제하기

--- articles-server/api/article/controllers/article.js
+++ articles-server/api/article/controllers/article.js
@@ -33,4 +33,25 @@ module.exports = {
     // 응답 반환
     return sanitizeEntity(entity, { model: strapi.models.article });
   },
+
+  async delete(ctx) {
+    const { id } = ctx.params; // URL 파라미터에서 id 추출
+    const article = await strapi.services.article.findOne({ id }); // id로 데이터 조회
+
+    // 데이터가 존재하지 않을 때
+    if (!article) {
+      return ctx.throw(404);
+    }
+
+    // 사용자 id와 article의 작성자 id가 일치하는지 확인
+    if (ctx.state.user.id !== article.user.id) {
+      return ctx.unauthorized(`You can't remove this entry`);
+    }
+
+    // 데이터 삭제
+    await strapi.services.article.delete({ id });
+
+    // 응답 반환
+    ctx.status = 204; // no content
+  },
 };
