7bcd2a52858e30198706f5c7ab43749e6ebb354c
ddojai <heungyeol87@gmail.com>
2023-05-10 23:35:37 +0900

    14.5.2 게시글 수정하기

--- articles-server/api/article/controllers/article.js
+++ articles-server/api/article/controllers/article.js
@@ -9,4 +9,28 @@ module.exports = {
     // 잘못된 필드 및 Private 값 제외하고 반환
     return sanitizeEntity(entity, { model: strapi.models.article });
   },
+
+  async update(ctx) {
+    const { id } = ctx.params; // URL 파라미터에서 id 추출
+    const article = await strapi.services.article.findOne({ id }); // id로 데이터 조회
+    // 데이터가 존재하지 않을 때
+    if (!article) {
+      return ctx.throw(404);
+    }
+    // user 정보는 변경할 수 없도록 처리
+    if (ctx.request.body.user) {
+      return ctx.throw(400, "user field cannot be changed");
+    }
+    // 사용자의 id와 article의 작성자 id가 일치하는지 확인
+    if (ctx.state.user.id !== article.user.id) {
+      return ctx.unauthorized(`You can't update this entry`);
+    }
+    // article 데이터 업데이트
+    const entity = await strapi.services.article.update(
+      { id },
+      ctx.request.body
+    );
+    // 응답 반환
+    return sanitizeEntity(entity, { model: strapi.models.article });
+  },
 };
